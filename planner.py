c310cfe2e1a59b15286fb204ae49eb4aca60bee5
kjamrit <96388355+kjamrit@users.noreply.github.com>
2022-02-11 11:15:32 +0530

    Setup

---
+++ planner.py
@@ -0,0 +1,18 @@
+
+
+class Courses():
+
+    def __init__(self,Name,Description,Units):
+        self.Name=Name
+        self.Description=Description
+        self.Units=Units
+
+course_1=Courses('BIO F110', 'BIOLOGY LABORATORY', 1)
+course_2=Courses('BITS F110', 'ENGINEERING GRAPHICS',2)
+course_3=Courses('BITS F111','THERMODYNAMICS',3)
+course_4=Courses('CHEM F110','CHEMISTRY LAB',1)
+course_5=Courses('CHEM F111','GEN CHEM',3)
+course_6=Courses('EEE F111','ELECTRICAL SCIENCES',3)
+course_7=Courses('MATH F111','MATHEMATICS 1',3)
+course_8=Courses('PHY F110','PHY LAB',1)
+course_9=Courses('PHY F111','MECHANICS OSCILLATIONS',3)
